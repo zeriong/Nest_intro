e881dabb22793d8474ae67d44bb4a14b6bdb36e9
zeriong <jaeryong95@gmail.com>
2022-12-04 16:36:54 +0900

    service생성

---
+++ src/movies/movies.service.ts
@@ -0,0 +1,39 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+
+@Injectable()
+export class MoviesService {
+  private movies: Movie[] = [];
+
+  getAll(): Movie[] {
+    return this.movies;
+  }
+  //가짜 데이터베이스이며, 진짜데이터베이스에서는 Query를 가져온다.
+
+  getOne(id: string): Movie {
+    const movie = this.movies.find((movie) => movie.id === +id);
+    if (!movie) {
+      throw new NotFoundException(`Movie with ID: ${id} not found.`);
+      //HttpException에서 확장된 NestJS의 제공기능. 예외처리 기능을 한다.
+    }
+    return movie;
+  }
+
+  deleteOne(id: string) {
+    this.getOne(id);
+    this.movies = this.movies.filter((movie) => movie.id !== +id);
+  }
+
+  create(movieData) {
+    this.movies.push({
+      id: this.movies.length + 1,
+      ...movieData,
+    });
+  }
+
+  update(id: string, updateData) {
+    const movie = this.getOne(id);
+    this.deleteOne(id);
+    this.movies.push({ ...movie, ...updateData });
+  }
+}
