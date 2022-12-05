9b9c1b75193c5508b01ba7ab5492d69159a081d7
zeriong <jaeryong95@gmail.com>
2022-12-05 21:37:48 +0900

    테스트환경을 서비스환경과 동일화, e2e테스트완료

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,17 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+        /*transform은 요청데이터를 필요한 데이터형태로 바꿔준다. Test도 마찬가지로 같은 환경을
+          만들어줘야테스트가 제대로 되기때문에 테스트에도 파이프를 적용해준다.*/
+      }),
+    );
     await app.init();
   });
 
@@ -19,6 +28,55 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcome to my Movie API');
+  });
+
+  describe('/movies', () => {
+    it('GET', () => {
+      return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
+    });
+    it('POST 201', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2000,
+          genres: ['test'],
+        })
+        .expect(201);
+    });
+    it('POST 400', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2000,
+          genres: ['test'],
+          other: 'thing',
+        })
+        .expect(400);
+    });
+    it('DELETE 404', () => {
+      return request(app.getHttpServer()).delete('/movies').expect(404);
+    });
+  });
+  describe('/movies/:id', () => {
+    it('GET 200', () => {
+      return request(app.getHttpServer()).get('/movies/1').expect(200);
+    });
+    it('PATCH 200', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({ title: 'Update', year: 2020, genres: ['love'] })
+        .expect(200);
+    });
+    it('DELETE 200', () => {
+      return request(app.getHttpServer()).delete('/movies/1').expect(200);
+    });
+
+    it.todo('DELETE 200');
+    it.todo('PATCH 200');
+    /* jest에서 제공하는 todo기능이다. 해야하는 반드시 테스트해야하는 기능이름으로 todo를
+    설정하면 테스트하면 터미널에 보여진다.(혹시나 빼먹을 수 있기 때문에 주석같이 표시하는 용도) */
   });
 });
